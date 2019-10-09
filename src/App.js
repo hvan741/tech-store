c57e2e65688b4a9279a6b08242cd1e5a86651673
unknown <hvan202@mail.ru>
2019-10-09 17:47:10 +0300

    Initialisation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col text-warning display-2">Hello world</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
